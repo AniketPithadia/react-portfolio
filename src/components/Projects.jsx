971b9b8a9bcb8200ebb613d3badf3c31df0c6289
Aniket <aniket@pop-os.localdomain>
2023-01-22 16:54:51 +0530

    files added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
-import WorkImg from "../assets/workImg.jpeg";
-import realEstate from "../assets/realestate.jpg";
+import animegram from "../assets/animegram.png";
+import fitnessapp from "../assets/fitnessapp.png";
+import youtube from "../assets/youtube.png";
 
 const Work = () => {
   return (
@@ -20,7 +21,7 @@ const Work = () => {
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
           {/* Grid Item 1 */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${animegram})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div cursor-pointer"
             data-aos="fade-down-left"
             data-aos-duration="1500"
@@ -28,15 +29,23 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100">
               <span className="text-2xl text-center flex justify-center font-bold text-white tracking-wider">
-                React JS Application
+                Animegram
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://animegram-jade.vercel.app/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
+                <a
+                  href="https://github.com/AniketPithadia/Animegram"
+                  rel="noreferrer"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
@@ -46,7 +55,7 @@ const Work = () => {
           </div>
           {/* Grid Item 2 */}
           <div
-            style={{ backgroundImage: `url(${realEstate})` }}
+            style={{ backgroundImage: `url(${youtube})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div cursor-pointer"
             data-aos="fade-down-left"
             data-aos-duration="1700"
@@ -72,7 +81,7 @@ const Work = () => {
           </div>
           {/* Grid Item 3 */}
           <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
+            style={{ backgroundImage: `url(${fitnessapp})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div cursor-pointer"
             data-aos="fade-down-left"
             data-aos-duration="1900"
@@ -80,93 +89,23 @@ const Work = () => {
             {/* Hover Effects */}
             <div className="opacity-0 group-hover:opacity-100 ">
               <span className="text-2xl text-center flex justify-center font-bold text-white tracking-wider">
-                React JS Application
+                Fitness App
               </span>
               <div className="pt-8 text-center">
-                <a href="/">
+                <a
+                  href="https://gym-app-eight.vercel.app/"
+                  rel="noreferrer"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 4 */}
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div cursor-pointer"
-            data-aos="fade-up-right"
-            data-aos-duration="2200"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl text-center flex justify-center  font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 5 */}
-          <div
-            style={{ backgroundImage: `url(${WorkImg})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div cursor-pointer"
-            data-aos="fade-up-right"
-            data-aos-duration="2500"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl text-center flex justify-center font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Code
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
-          {/* Grid Item 6 */}
-          <div
-            style={{ backgroundImage: `url(${realEstate})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div cursor-pointer"
-            data-aos="fade-up-right"
-            data-aos-duration="2700"
-          >
-            {/* Hover Effects */}
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl text-center flex justify-center font-bold text-white tracking-wider">
-                React JS Application
-              </span>
-              <div className="pt-8 text-center">
-                <a href="/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="/">
+                <a
+                  href="https://github.com/AniketPithadia/Gym-app"
+                  rel="noreferrer"
+                  target="_blank"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
